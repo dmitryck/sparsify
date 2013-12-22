d96fd6f50f81c5ad6806650155496ad1cce1ea41
Ryan Biesemeyer <ryan@simplymeasured.com>
2013-12-22 17:02:34 +0000

    be explicit about private methods on Kernel

--- lib/sparsify/core_ext/kernel.rb
+++ lib/sparsify/core_ext/kernel.rb
@@ -2,17 +2,17 @@
 
 module Kernel
 
-  private
-
   # @see Sparsify#sparse
   # @api public
   def Sparsify(hsh, options = {})
     Sparsify.sparse(hsh, options)
   end
+  private :Sparsify
 
   # @see Sparsify#unsparse
   # @api public
   def Unsparsify(hsh, options = {})
     Sparsify.unsparse(hsh, options)
   end
+  private :Unsparsify
 end
